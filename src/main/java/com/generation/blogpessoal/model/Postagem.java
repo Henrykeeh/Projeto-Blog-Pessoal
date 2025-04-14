80febad7ee6f180154db16cfd157c8fabe37d6f0
Henrykeeh <c.henrique.silvabarbosa@gmail.com>
2025-04-14 12:11:33 -0300

    Criados os pacotes Controller, Model e Repository

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,63 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity //@Entity transforma a classe em tabela através do JPA
+@Table(name = "tb_postagens") //@Table define o nome para a tabela, caso não coloque o table ele criará com um nome genérico
+public class Postagem {
+	
+	//Variáveis que se transformarão em colunas no MySQL
+	@Id // Anotação para indicar a PK - primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Anotação para regra de auto increment no ID
+	private Long id; // o Long se transformará em BIGINT no MySQL
+	
+	@NotBlank // Anotação para não nulo e não vazio (ou seja, não aceita nem mesmo apenas espaços, diferente do NotNull
+	@Size(min = 3, max = 100) // Anotação para o VARCHAR, define o tamanho necessário, minimo e máximo da String
+	private String titulo;
+	
+	@NotBlank
+	@Size(min = 10, max = 1000)
+	private String texto;
+	
+	@UpdateTimestamp // Toda postagem criada pegarao automaticamente a data e hora atual do usuário
+	private LocalDate data;
+	
+	//Getters e Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+}
